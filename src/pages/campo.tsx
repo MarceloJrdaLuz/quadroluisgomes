227ea414c7f8937185b766ec930f37b16bf0316e
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2022-03-29 23:32:25 -0300

    Ajuste icone campo

--- src/pages/campo.tsx
+++ src/pages/campo.tsx
@@ -15,8 +15,8 @@ export default function Campo (){
     }
     return !pdfShow ? (
         <>
-        <HeadComponent title="Designações de Campo"/>
-        <LayoutPrincipal heightConteudo={'1/2'} header className="bg-home bg-left-bottom bg-cover lg:bg-right" textoHeader="Designações de Campo" >
+        <HeadComponent title="Designações de Campo" urlMiniatura="https://bituruna.netlify.app/images/campolight.png"/>
+        <LayoutPrincipal heightConteudo={'1/2'} header className="bg-campo bg-center bg-cover lg:bg-right" textoHeader="Designações de Campo" >
                
             <div className="linha bg-gray-500 mt-2 w-full h-0.5 md:w-4/5"></div>
             <Botao onClick={() => {setPdfShow(true)}}texto='Designações de Campo'/>
