89ca1a5ff7e6ee84a2387c989e31866855899be2
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2022-12-23 14:39:37 -0300

    Label

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -6,5 +6,5 @@ interface LabelProps {
 
 export default function Label(props: LabelProps) {
     return (
-        <label className={`rounded-lg font-semibold absolute top-0 p-4 text-lg ${props.invalido ? 'text-red-700 ': 'text-black'} -z-10 right-2 duration-300 origin-0 ${props.readonly ? 'bg-gray-300' : 'bg-gray-200'}`}>{props.texto}</label>)
+        <label className={`rounded-lg font-semibold absolute top-0 p-4 text-lg ${props.invalido ? 'text-red-700 ': 'text-black'} -z-0 right-2 duration-300 origin-0 ${props.readonly ? 'bg-gray-300' : 'bg-gray-200'}`}>{props.texto}</label>)
 }
